867867f2e933423117a179a114450ddae4f46d97
chandrakala1 <dp6302728379@gmail.com>
2025-05-01 16:56:06 +0530

    version 02

---
+++ if.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+	echo "enter number01:"
+	read a
+	echo "enter number02:"
+	read b
+	echo "enter number03:"
+	read c
+	echo "enter number04:"
+	read d
+	if ((a>d && a>C && a>d));then
+		echo "$a is the largest number:"
+	elif ((b>c && b>d));then
+		echo "$b is the largest number:"
+	elif ((c>d));then
+		echo "$c is the largest number:"
+	else
+		echo "$d is the largest number:"
+	fi
+
+
